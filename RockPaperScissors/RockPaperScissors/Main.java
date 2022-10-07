493a461653e8e2722cc6978d0605068a3308f1ae
hlsylmz <115081259+hlsylmz@users.noreply.github.com>
2022-10-08 00:23:12 +0200

    Add files via upload

---
+++ RockPaperScissors/RockPaperScissors/Main.java
@@ -0,0 +1,10 @@
+package RockPaperScissors.RockPaperScissors;
+
+import RockPaperScissors.RockPaperScissors;
+
+public class Main {
+    public static void main(String[] args) {
+        RockPaperScissors.RockPaperScissors.RockPaperScissors.RockPaperScissors.RockPaperScissors game = new RockPaperScissors.RockPaperScissors.RockPaperScissors.RockPaperScissors.RockPaperScissors("lucas", 0);
+       game.playGame();
+    }
+}
