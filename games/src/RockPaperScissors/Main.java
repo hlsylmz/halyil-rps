043096533cd0487fd386349744885b19e2bbf666
hlsylmz <halisyilmaz@gmail.com>
2022-10-11 20:49:10 +0200

    third commit

---
+++ games/src/RockPaperScissors/Main.java
@@ -0,0 +1,8 @@
+package RockPaperScissors;
+
+public class Main {
+    public static void main(String[] args) {
+        RockPaperScissors game = new RockPaperScissors("lucas", 0);
+       game.playGame();
+    }
+}
