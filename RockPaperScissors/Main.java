b4c390de7b95efd126e0f0408c3c79e8c29855e3
hlsylmz <115081259+hlsylmz@users.noreply.github.com>
2022-10-12 20:43:18 +0200

    Delete Main.java

--- RockPaperScissors/Main.java
+++
@@ -1,8 +0,0 @@
-package RockPaperScissors;
-
-public class Main {
-    public static void main(String[] args) {
-        RockPaperScissors game = new RockPaperScissors("lucas", 0);
-       game.playGame();
-    }
-}
